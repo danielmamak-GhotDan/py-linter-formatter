8059cbb296e9f4cab689af7268874a5e8f5d4a96
Daniel Mamak <daniel.mamak@gmail.com>
2026-05-20 17:56:52 +0200

    Refaktoryzacja kodu

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 def format_linter_error(error: dict) -> dict:
     """Formatowanie pojedyńczych błędów"""
-    
     return {
         "line": error["line_number"],
-        "column": error["column_number"], 
+        "column": error["column_number"],
         "message": error["text"],
-        "name": error["code"], 
+        "name": error["code"],
         "source": "flake8"
     }
 
