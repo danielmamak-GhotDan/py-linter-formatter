346858e4256fc0bf54a005538e9a727b43802240
Daniel Mamak <daniel.mamak@gmail.com>
2026-05-20 17:54:54 +0200

    Zapisanie projektu

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 def format_linter_error(error: dict) -> dict:
     """Formatowanie pojedyńczych błędów"""
+    
     return {
         "line": error["line_number"],
-        "column": error["column_number"],
+        "column": error["column_number"], 
         "message": error["text"],
-        "name": error["code"],
+        "name": error["code"], 
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {
-        "file_path": file_path,
-        "errors": [format_linter_error(error) for error in errors]
-    }
+    # write your code here
+    pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    # write your code here
+    pass
