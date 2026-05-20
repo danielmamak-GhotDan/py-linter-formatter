cbebd3c77f98800aba3066827c9cc9bb45422549
Daniel Mamak <daniel.mamak@gmail.com>
2026-05-20 17:53:54 +0200

    Dodanie docstring

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 def format_linter_error(error: dict) -> dict:
-    def format_linter_error(error: dict) -> dict:
+    """Formatowanie pojedyńczych błędów"""
     return {
         "line": error["line_number"],
-        "column": error["column_number"], 
+        "column": error["column_number"],
         "message": error["text"],
-        "name": error["code"], 
+        "name": error["code"],
         "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "file_path": file_path,
+        "errors": [format_linter_error(error) for error in errors]
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
