cef45d6e24a389422ab71fd72c525b2542b70697
Daniel Mamak <daniel.mamak@gmail.com>
2026-05-23 22:05:45 +0200

    Ostateczna wersja kodu v2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 def format_linter_error(error: dict) -> dict:
-    """Formatowanie pojedyńczych błędów"""
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,7 +9,6 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    """Sprawdzanie obecności błędów w projekcie"""
     return {
         "errors": [
             format_linter_error(error) for error in errors
@@ -21,7 +19,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    """Uporządkowana lista kodów-ostatateczna wersja"""
     return [
         format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
